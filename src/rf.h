36065e63928e454ca3bcfacb445767c145257b4f
Zihao Xu <zxql2015@mymail.pomona.edu>
2017-06-05 09:51:22 -0700

    adding multinomial as functions to use, and include the functions in the header

--- src/rf.h
+++ src/rf.h
@@ -1,15 +1,15 @@
 /*******************************************************************
-   Copyright (C) 2001-7 Leo Breiman, Adele Cutler and Merck & Co., Inc.
-  
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License
-   as published by the Free Software Foundation; either version 2
-   of the License, or (at your option) any later version.
+ Copyright (C) 2001-7 Leo Breiman, Adele Cutler and Merck & Co., Inc.
  
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.                            
+ This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.                            
 *******************************************************************/
 #ifndef RF_H
 #define RF_H
@@ -22,14 +22,14 @@
 #define swapInt(a, b) ((a ^= b), (b ^= a), (a ^= b))
 /*
 void classRF(double *x, int *dimx, int *cl, int *ncl, int *cat, int *maxcat, 
-	int *sampsize, int *Options, int *ntree, int *nvar,
-	int *ipi, double *pi, double *cut, int *nodesize, 
-        int *outcl, int *counttr, double *prox, 
-	double *imprt, double *, double *impmat, int *nrnodes, int *ndbigtree, 
-	int *nodestatus, int *bestvar, int *treemap, int *nodeclass,
-	double *xbestsplit, double *pid, double *errtr, 
-	int *testdat, double *xts, int *clts, int *nts, double *countts,
-	int *outclts, int *labelts, double *proxts, double *errts);
+             int *sampsize, int *Options, int *ntree, int *nvar,
+int *ipi, double *pi, double *cut, int *nodesize, 
+int *outcl, int *counttr, double *prox, 
+double *imprt, double *, double *impmat, int *nrnodes, int *ndbigtree, 
+int *nodestatus, int *bestvar, int *treemap, int *nodeclass,
+double *xbestsplit, double *pid, double *errtr, 
+int *testdat, double *xts, int *clts, int *nts, double *countts,
+int *outclts, int *labelts, double *proxts, double *errts);
 */
 
 void normClassWt(int *cl, const int nsample, const int nclass, 
@@ -43,26 +43,26 @@ void classForest(int *mdim, int *ntest, int *nclass, int *maxcat,
                  int *keepPred, int *prox, double *proxmatrix, int *nodes);
 
 void regTree(double *x, double *y, int mdim, int nsample, 
-	     int *lDaughter, int *rDaughter, double *upper, double *avnode, 
+             int *lDaughter, int *rDaughter, double *upper, double *avnode, 
              int *nodestatus, int nrnodes, int *treeSize, int nthsize, 
              int mtry, int *mbest, int *cat, double *tgini, int *varUsed);
 
 void findBestSplit(double *x, int *jdex, double *y, int mdim, int nsample, 
-		   int ndstart, int ndend, int *msplit, double *decsplit, 
-		   double *ubest, int *ndendl, int *jstat, int mtry,
-		   double sumnode, int nodecnt, int *cat);
+                   int ndstart, int ndend, int *msplit, double *decsplit, 
+                   double *ubest, int *ndendl, int *jstat, int mtry,
+                   double sumnode, int nodecnt, int *cat);
 
 void predictRegTree(double *x, int nsample, int mdim, 
-		    int *lDaughter, int *rDaughter, int *nodestatus, 
+                    int *lDaughter, int *rDaughter, int *nodestatus, 
                     double *ypred, double *split, double *nodepred, 
                     int *splitVar, int treeSize, int *cat, int maxcat,
                     int *nodex);
 
 void predictClassTree(double *x, int n, int mdim, int *treemap,
-		      int *nodestatus, double *xbestsplit,
-		      int *bestvar, int *nodeclass,
-		      int ndbigtree, int *cat, int nclass,
-		      int *jts, int *nodex, int maxcat);
+                      int *nodestatus, double *xbestsplit,
+                      int *bestvar, int *nodeclass,
+                      int ndbigtree, int *cat, int nclass,
+                      int *jts, int *nodex, int maxcat);
 
 double pack(const int l, const int *icat);
 void unpack(const double pack, const int nBits, int *icat);
@@ -71,35 +71,34 @@ void zeroInt(int *x, int length);
 void zeroDouble(double *x, int length);
 void createClass(double *x, int realN, int totalN, int mdim);
 void prepare(int *cl, const int nsample, const int nclass, const int ipi, 
-	     double *pi, double *pid, int *nc, double *wtt);
+             double *pi, double *pid, int *nc, double *wtt);
 void makeA(double *x, const int mdim, const int nsample, int *cat, int *a, 
            int *b);
 void modA(int *a, int *nuse, const int nsample, const int mdim, int *cat, 
           const int maxcat, int *ncase, int *jin);
 void Xtranslate(double *x, int mdim, int nrnodes, int nsample, 
-		int *bestvar, int *bestsplit, int *bestsplitnext,
-		double *xbestsplit, int *nodestatus, int *cat, int treeSize);
+                int *bestvar, int *bestsplit, int *bestsplitnext,
+                double *xbestsplit, int *nodestatus, int *cat, int treeSize);
 void permuteOOB(int m, double *x, int *in, int nsample, int mdim);
 void computeProximity(double *prox, int oobprox, int *node, int *inbag, 
                       int *oobpair, int n);
+void rmultinomBLB(int n, int K, double* prob, int* rN);
+int rbinomBLB(double p, int n);
+double rand_val(int seed);
+
 
 /* Template of Fortran subroutines to be called from the C wrapper */
 extern void F77_NAME(buildtree)(int *a, int *b, int *cl, int *cat, 
-				int *maxcat, int *mdim, int *nsample, 
-				int *nclass, int *treemap, int *bestvar, 
-				int *bestsplit, int *bestsplitnext, 
-				double *tgini, int *nodestatus, int *nodepop, 
-				int *nodestart, double *classpop, 
-				double *tclasspop, double *tclasscat, 
-				int *ta, int *nrnodes, int *, 
-				int *, int *, int *, int *, int *, int *, 
-				double *, double *, double *,
-				int *, int *, int *); 
-
-/* Define new functions:*/
-double rand_val(int seed);
-int rbinom(double p, int n);
-void rmultinom(int n, int K, double* prob, int* rN);
+                     int *maxcat, int *mdim, int *nsample, 
+                     int *nclass, int *treemap, int *bestvar, 
+                     int *bestsplit, int *bestsplitnext, 
+                     double *tgini, int *nodestatus, int *nodepop, 
+                     int *nodestart, double *classpop, 
+                     double *tclasspop, double *tclasscat, 
+                     int *ta, int *nrnodes, int *, 
+                     int *, int *, int *, int *, int *, int *, 
+                     double *, double *, double *,
+                     int *, int *, int *); 
 
 /* maximum number of categories allowed in categorical predictors */
 #define MAX_CAT 53
--- src/rfutils.c
+++ src/rfutils.c
@@ -1,295 +1,291 @@
 /*******************************************************************
-   Copyright (C) 2001-2012 Leo Breiman, Adele Cutler and Merck & Co., Inc.
-
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License
-   as published by the Free Software Foundation; either version 2
-   of the License, or (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+ Copyright (C) 2001-2012 Leo Breiman, Adele Cutler and Merck & Co., Inc.
+ This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 *******************************************************************/
 #include <R.h>
 #include "rf.h"
 
 
 void zeroInt(int *x, int length) {
-    memset(x, 0, length * sizeof(int));
+  memset(x, 0, length * sizeof(int));
 }
 
 void zeroDouble(double *x, int length) {
-    memset(x, 0, length * sizeof(double));
+  memset(x, 0, length * sizeof(double));
 }
 
 void createClass(double *x, int realN, int totalN, int mdim) {
-/* Create the second class by bootstrapping each variable independently. */
-    int i, j, k;
-    for (i = realN; i < totalN; ++i) {
-        for (j = 0; j < mdim; ++j) {
-            k = (int) (unif_rand() * realN);
-            x[j + i * mdim] = x[j + k * mdim];
-        }
+  /* Create the second class by bootstrapping each variable independently. */
+  int i, j, k;
+  for (i = realN; i < totalN; ++i) {
+    for (j = 0; j < mdim; ++j) {
+      k = (int) (unif_rand() * realN);
+      x[j + i * mdim] = x[j + k * mdim];
     }
+  }
 }
 
 void normClassWt(int *cl, const int nsample, const int nclass,
                  const int useWt, double *classwt, int *classFreq) {
-    int i;
-    double sumwt = 0.0;
-
-    if (useWt) {
-        /* Normalize user-supplied weights so they sum to one. */
-        for (i = 0; i < nclass; ++i) sumwt += classwt[i];
-        for (i = 0; i < nclass; ++i) classwt[i] /= sumwt;
-    } else {
-        for (i = 0; i < nclass; ++i) {
-            classwt[i] = ((double) classFreq[i]) / nsample;
-        }
-    }
+  int i;
+  double sumwt = 0.0;
+  
+  if (useWt) {
+    /* Normalize user-supplied weights so they sum to one. */
+    for (i = 0; i < nclass; ++i) sumwt += classwt[i];
+    for (i = 0; i < nclass; ++i) classwt[i] /= sumwt;
+  } else {
     for (i = 0; i < nclass; ++i) {
-        classwt[i] = classFreq[i] ? classwt[i] * nsample / classFreq[i] : 0.0;
+      classwt[i] = ((double) classFreq[i]) / nsample;
     }
+  }
+  for (i = 0; i < nclass; ++i) {
+    classwt[i] = classFreq[i] ? classwt[i] * nsample / classFreq[i] : 0.0;
+  }
 }
 
 void makeA(double *x, const int mdim, const int nsample, int *cat, int *a,
            int *b) {
-    /* makeA() constructs the mdim by nsample integer array a.  For each
-       numerical variable with values x(m, n), n=1, ...,nsample, the x-values
-       are sorted from lowest to highest.  Denote these by xs(m, n).  Then
-       a(m,n) is the case number in which xs(m, n) occurs. The b matrix is
-       also contructed here.  If the mth variable is categorical, then
-       a(m, n) is the category of the nth case number. */
-    int i, j, n1, n2, *index;
-    double *v;
-
-    v     = (double *) Calloc(nsample, double);
-    index = (int *) Calloc(nsample, int);
-
-    for (i = 0; i < mdim; ++i) {
-        if (cat[i] == 1) { /* numerical predictor */
-            for (j = 0; j < nsample; ++j) {
-                v[j] = x[i + j * mdim];
-                index[j] = j + 1;
-            }
-            R_qsort_I(v, index, 1, nsample);
-
-            /*  this sorts the v(n) in ascending order. index(n) is the case
-                number of that v(n) nth from the lowest (assume the original
-                case numbers are 1,2,...).  */
-            for (j = 0; j < nsample-1; ++j) {
-                n1 = index[j];
-                n2 = index[j + 1];
-                a[i + j * mdim] = n1;
-                if (j == 0) b[i + (n1-1) * mdim] = 1;
-                b[i + (n2-1) * mdim] =  (v[j] < v[j + 1]) ?
-                    b[i + (n1-1) * mdim] + 1 : b[i + (n1-1) * mdim];
-            }
-            a[i + (nsample-1) * mdim] = index[nsample-1];
-        } else { /* categorical predictor */
-            for (j = 0; j < nsample; ++j)
-                a[i + j*mdim] = (int) x[i + j * mdim];
-        }
+  /* makeA() constructs the mdim by nsample integer array a.  For each
+  numerical variable with values x(m, n), n=1, ...,nsample, the x-values
+  are sorted from lowest to highest.  Denote these by xs(m, n).  Then
+  a(m,n) is the case number in which xs(m, n) occurs. The b matrix is
+  also contructed here.  If the mth variable is categorical, then
+  a(m, n) is the category of the nth case number. */
+  int i, j, n1, n2, *index;
+  double *v;
+  
+  v     = (double *) Calloc(nsample, double);
+  index = (int *) Calloc(nsample, int);
+  
+  for (i = 0; i < mdim; ++i) {
+    if (cat[i] == 1) { /* numerical predictor */
+  for (j = 0; j < nsample; ++j) {
+    v[j] = x[i + j * mdim];
+    index[j] = j + 1;
+  }
+  R_qsort_I(v, index, 1, nsample);
+      
+      /*  this sorts the v(n) in ascending order. index(n) is the case
+      number of that v(n) nth from the lowest (assume the original
+      case numbers are 1,2,...).  */
+      for (j = 0; j < nsample-1; ++j) {
+        n1 = index[j];
+        n2 = index[j + 1];
+        a[i + j * mdim] = n1;
+        if (j == 0) b[i + (n1-1) * mdim] = 1;
+        b[i + (n2-1) * mdim] =  (v[j] < v[j + 1]) ?
+        b[i + (n1-1) * mdim] + 1 : b[i + (n1-1) * mdim];
+      }
+      a[i + (nsample-1) * mdim] = index[nsample-1];
+    } else { /* categorical predictor */
+      for (j = 0; j < nsample; ++j)
+        a[i + j*mdim] = (int) x[i + j * mdim];
     }
-    Free(index);
-    Free(v);
+  }
+  Free(index);
+  Free(v);
 }
 
 
 void modA(int *a, int *nuse, const int nsample, const int mdim,
-	  int *cat, const int maxcat, int *ncase, int *jin) {
-    int i, j, k, m, nt;
-
-    *nuse = 0;
-    for (i = 0; i < nsample; ++i) if (jin[i]) (*nuse)++;
-
-    for (i = 0; i < mdim; ++i) {
-      k = 0;
-      nt = 0;
-      if (cat[i] == 1) {
-          for (j = 0; j < nsample; ++j) {
-              if (jin[a[i + k * mdim] - 1]) {
-                  a[i + nt * mdim] = a[i + k * mdim];
-                  k++;
-              } else {
-                  for (m = 0; m < nsample - k; ++m) {
-                      if (jin[a[i + (k + m) * mdim] - 1]) {
-                          a[i + nt * mdim] = a[i + (k + m) * mdim];
-                          k += m + 1;
-                          break;
-                      }
-                  }
-              }
-              nt++;
-              if (nt >= *nuse) break;
+          int *cat, const int maxcat, int *ncase, int *jin) {
+  int i, j, k, m, nt;
+  
+  *nuse = 0;
+  for (i = 0; i < nsample; ++i) if (jin[i]) (*nuse)++;
+  
+  for (i = 0; i < mdim; ++i) {
+    k = 0;
+    nt = 0;
+    if (cat[i] == 1) {
+      for (j = 0; j < nsample; ++j) {
+        if (jin[a[i + k * mdim] - 1]) {
+          a[i + nt * mdim] = a[i + k * mdim];
+          k++;
+        } else {
+          for (m = 0; m < nsample - k; ++m) {
+            if (jin[a[i + (k + m) * mdim] - 1]) {
+              a[i + nt * mdim] = a[i + (k + m) * mdim];
+              k += m + 1;
+              break;
+            }
           }
+        }
+        nt++;
+        if (nt >= *nuse) break;
       }
     }
-    if (maxcat > 1) {
-        k = 0;
-        nt = 0;
-        for (i = 0; i < nsample; ++i) {
-            if (jin[k]) {
-                k++;
-                ncase[nt] = k;
-            } else {
-                for (j = 0; j < nsample - k; ++j) {
-                    if (jin[k + j]) {
-                        ncase[nt] = k + j + 1;
-                        k += j + 1;
-                        break;
-                    }
-                }
-            }
-            nt++;
-            if (nt >= *nuse) break;
+  }
+  if (maxcat > 1) {
+    k = 0;
+    nt = 0;
+    for (i = 0; i < nsample; ++i) {
+      if (jin[k]) {
+        k++;
+        ncase[nt] = k;
+      } else {
+        for (j = 0; j < nsample - k; ++j) {
+          if (jin[k + j]) {
+            ncase[nt] = k + j + 1;
+            k += j + 1;
+            break;
+          }
         }
+      }
+      nt++;
+      if (nt >= *nuse) break;
     }
+  }
 }
 
 void Xtranslate(double *x, int mdim, int nrnodes, int nsample,
-		int *bestvar, int *bestsplit, int *bestsplitnext,
-		double *xbestsplit, int *nodestatus, int *cat, int treeSize) {
-/*
- this subroutine takes the splits on numerical variables and translates them
- back into x-values.  It also unpacks each categorical split into a
- 32-dimensional vector with components of zero or one--a one indicates that
- the corresponding category goes left in the split.
-*/
-
-    int i, m;
-
-    for (i = 0; i < treeSize; ++i) {
-	if (nodestatus[i] == 1) {
-	    m = bestvar[i] - 1;
-	    if (cat[m] == 1) {
-		xbestsplit[i] = 0.5 * (x[m + (bestsplit[i] - 1) * mdim] +
-				       x[m + (bestsplitnext[i] - 1) * mdim]);
-	    } else {
-		xbestsplit[i] = (double) bestsplit[i];
-	    }
-	}
+                int *bestvar, int *bestsplit, int *bestsplitnext,
+                double *xbestsplit, int *nodestatus, int *cat, int treeSize) {
+  /*
+  this subroutine takes the splits on numerical variables and translates them
+  back into x-values.  It also unpacks each categorical split into a
+  32-dimensional vector with components of zero or one--a one indicates that
+  the corresponding category goes left in the split.
+  */
+  
+  int i, m;
+  
+  for (i = 0; i < treeSize; ++i) {
+    if (nodestatus[i] == 1) {
+      m = bestvar[i] - 1;
+      if (cat[m] == 1) {
+        xbestsplit[i] = 0.5 * (x[m + (bestsplit[i] - 1) * mdim] +
+          x[m + (bestsplitnext[i] - 1) * mdim]);
+      } else {
+        xbestsplit[i] = (double) bestsplit[i];
+      }
     }
+  }
 }
 
 void permuteOOB(int m, double *x, int *in, int nsample, int mdim) {
-/* Permute the OOB part of a variable in x.
- * Argument:
- *   m: the variable to be permuted
- *   x: the data matrix (variables in rows)
- *   in: vector indicating which case is OOB
- *   nsample: number of cases in the data
- *   mdim: number of variables in the data
- */
-    double *tp, tmp;
-    int i, last, k, nOOB = 0;
-
-    tp = (double *) Calloc(nsample, double);
-
-    for (i = 0; i < nsample; ++i) {
-		/* make a copy of the OOB part of the data into tp (for permuting) */
-		if (in[i] == 0) {
-            tp[nOOB] = x[m + i*mdim];
-            nOOB++;
-        }
+  /* Permute the OOB part of a variable in x.
+  * Argument:
+  *   m: the variable to be permuted
+  *   x: the data matrix (variables in rows)
+  *   in: vector indicating which case is OOB
+  *   nsample: number of cases in the data
+  *   mdim: number of variables in the data
+  */
+  double *tp, tmp;
+  int i, last, k, nOOB = 0;
+  
+  tp = (double *) Calloc(nsample, double);
+  
+  for (i = 0; i < nsample; ++i) {
+    /* make a copy of the OOB part of the data into tp (for permuting) */
+    if (in[i] == 0) {
+      tp[nOOB] = x[m + i*mdim];
+      nOOB++;
     }
-    /* Permute tp */
-    last = nOOB;
-    for (i = 0; i < nOOB; ++i) {
-		k = (int) last * unif_rand();
-		tmp = tp[last - 1];
-		tp[last - 1] = tp[k];
-		tp[k] = tmp;
-		last--;
+  }
+  /* Permute tp */
+  last = nOOB;
+  for (i = 0; i < nOOB; ++i) {
+    k = (int) last * unif_rand();
+    tmp = tp[last - 1];
+    tp[last - 1] = tp[k];
+    tp[k] = tmp;
+    last--;
+  }
+  
+  /* Copy the permuted OOB data back into x. */
+  nOOB = 0;
+  for (i = 0; i < nsample; ++i) {
+    if (in[i] == 0) {
+      x[m + i*mdim] = tp[nOOB];
+      nOOB++;
     }
-
-    /* Copy the permuted OOB data back into x. */
-    nOOB = 0;
-    for (i = 0; i < nsample; ++i) {
-		if (in[i] == 0) {
-            x[m + i*mdim] = tp[nOOB];
-            nOOB++;
-		}
-    }
-    Free(tp);
+  }
+  Free(tp);
 }
 
 /* Compute proximity. */
 void computeProximity(double *prox, int oobprox, int *node, int *inbag,
                       int *oobpair, int n) {
-/* Accumulate the number of times a pair of points fall in the same node.
-   prox:    n x n proximity matrix
-   oobprox: should the accumulation only count OOB cases? (0=no, 1=yes)
-   node:    vector of terminal node labels
-   inbag:   indicator of whether a case is in-bag
-   oobpair: matrix to accumulate the number of times a pair is OOB together
-   n:       total number of cases
-*/
-    int i, j;
-    for (i = 0; i < n; ++i) {
-        for (j = i+1; j < n; ++j) {
-            if (oobprox) {
-                if (! (inbag[i] > 0 || inbag[j] > 0) ) {
-                    oobpair[j*n + i] ++;
-                    oobpair[i*n + j] ++;
-                    if (node[i] == node[j]) {
-                        prox[j*n + i] += 1.0;
-                        prox[i*n + j] += 1.0;
-                    }
-                }
-            } else {
-                if (node[i] == node[j]) {
-                    prox[j*n + i] += 1.0;
-                    prox[i*n + j] += 1.0;
-                }
-            }
+  /* Accumulate the number of times a pair of points fall in the same node.
+  prox:    n x n proximity matrix
+  oobprox: should the accumulation only count OOB cases? (0=no, 1=yes)
+  node:    vector of terminal node labels
+  inbag:   indicator of whether a case is in-bag
+  oobpair: matrix to accumulate the number of times a pair is OOB together
+  n:       total number of cases
+  */
+  int i, j;
+  for (i = 0; i < n; ++i) {
+    for (j = i+1; j < n; ++j) {
+      if (oobprox) {
+        if (! (inbag[i] > 0 || inbag[j] > 0) ) {
+          oobpair[j*n + i] ++;
+          oobpair[i*n + j] ++;
+          if (node[i] == node[j]) {
+            prox[j*n + i] += 1.0;
+            prox[i*n + j] += 1.0;
+          }
         }
+      } else {
+        if (node[i] == node[j]) {
+          prox[j*n + i] += 1.0;
+          prox[i*n + j] += 1.0;
+        }
+      }
     }
+  }
 }
 
 double pack(const int nBits, const int *bits) {
-    int i = nBits - 1;
-	double pack = bits[i];
-    for (i = nBits - 1; i > 0; --i) pack = 2.0 * pack + bits[i - 1];
-    return(pack);
+  int i = nBits - 1;
+  double pack = bits[i];
+  for (i = nBits - 1; i > 0; --i) pack = 2.0 * pack + bits[i - 1];
+  return(pack);
 }
 
 void unpack(const double pack, const int nBits, int *bits) {
-    int i;
-    double x = pack;
-    for (i = 0; i <= nBits; ++i) {
-    	bits[i] = ((unsigned long) x & 1) ? 1 : 0;
-    	x = x / 2;
-    }
+  int i;
+  double x = pack;
+  for (i = 0; i <= nBits; ++i) {
+    bits[i] = ((unsigned long) x & 1) ? 1 : 0;
+    x = x / 2;
+  }
 }
 
 /*
 unsigned int pack(int nBits, int *bits) {
-    int i = nBits;
-	unsigned int pack = 0;
-    while (--i >= 0) pack += bits[i] << i;
-    return(pack);
+int i = nBits;
+unsigned int pack = 0;
+while (--i >= 0) pack += bits[i] << i;
+return(pack);
 }
-
 void unpack(int nBits, unsigned int pack, int *bits) {
-    int i;
-    for (i = 0; i < nBits; pack >>= 1, ++i) bits[i] = pack & 1;
+int i;
+for (i = 0; i < nBits; pack >>= 1, ++i) bits[i] = pack & 1;
 }
 */
 
 void F77_NAME(unpack)(double *pack, int *nBits, int *bits) {
-	unpack(*pack, *nBits, bits);
+  unpack(*pack, *nBits, bits);
 }
 
 
-/* rmultinom funciton: */
+/* rmultinomBLB funciton:*/
 // @ param n:       sum of the M_i
 // @ param K:       length of the probs vector
 // @ param probs:   vector of probs associated with each observation
 // @ param rN:      the resulting vector of M_i
-
-void rmultinom(int n, int K, double* prob, int* rN)
+void rmultinomBLB(int n, int K, double* prob, int* rN)
   /* `Return' vector  rN[1:K] {K := length(prob)}
   *  where rN[j] ~ Bin(n, prob[j]) ,  sum_j rN[j] == n,  sum_j prob[j] == 1,
   */
@@ -303,10 +299,10 @@ void rmultinom(int n, int K, double* prob, int* rN)
   result. */
   
   //#ifdef MATHLIB_STANDALONE
-  //    if (K < 1) { ML_ERROR(ME_DOMAIN, "rmultinom"); return;}
+  //    if (K < 1) { ML_ERROR(ME_DOMAIN, "rmultinomBLB"); return;}
   //    if (n < 0)  ML_ERR_ret_NAN(0);
   //#else
-  //    if (K == NA_INTEGER || K < 1) { ML_ERROR(ME_DOMAIN, "rmultinom"); return;}
+  //    if (K == NA_INTEGER || K < 1) { ML_ERROR(ME_DOMAIN, "rmultinomBLB"); return;}
   //    if (n == NA_INTEGER || n < 0)  ML_ERR_ret_NAN(0);
   //#endif
   
@@ -320,10 +316,10 @@ void rmultinom(int n, int K, double* prob, int* rN)
     rN[k] = 0;
   }
   //    if(fabs((double)(p_tot - 1.)) > 1e-7)
-  //        MATHLIB_ERROR(_("rbinom: probability sum should be 1, but is %g"),
+  //        MATHLIB_ERROR(_("rbinomBLB: probability sum should be 1, but is %g"),
   //                      (double) p_tot);
   if (n == 0) return;
-  if (K == 1 && p_tot == 0.) return;/* trivial border case: do as rbinom */
+  if (K == 1 && p_tot == 0.) return;/* trivial border case: do as rbinomBLB */
   
   /* Generate the first K-1 obs. via binomials */
   
@@ -331,7 +327,7 @@ void rmultinom(int n, int K, double* prob, int* rN)
   if(prob[k]) {
     pp = (double)(prob[k] / p_tot);
     // printf("[%d] %.17f\n", k+1, pp);
-    rN[k] = ((pp < 1.) ? rbinom(pp, n) :
+    rN[k] = ((pp < 1.) ? rbinomBLB(pp, n) :
                /*>= 1; > 1 happens because of rounding */
                n);
     n -= rN[k];
@@ -346,7 +342,7 @@ void rmultinom(int n, int K, double* prob, int* rN)
 
 
 
-int rbinom(double p, int n)
+int rbinomBLB(double p, int n)
 {
   int    bin_value;             // Computed Binomial value to be returned
   int    i;                     // Loop counter
@@ -396,4 +392,3 @@ double rand_val(int seed)
   // Return a random value between 0.0 and 1.0
   return((double) x / m);
 }
-
